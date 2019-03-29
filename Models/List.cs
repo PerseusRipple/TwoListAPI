1201b27fc6f6df263fd159cede3c9d61cc48525b
Will Warren <willwarrenmail@gmail.com>
2019-03-29 12:54:39 -0400

    Created Two List Table Together

---
+++ Models/List.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TwoListAPI.Models
+{
+  public class List
+  {
+    public int Id { get; set; }
+
+    public string ListItem { get; set; }
+
+    public bool IsDone { get; set; } = false;
+
+    public DateTime? DateOfCompletion { get; set; } = null;
+  }
+}
