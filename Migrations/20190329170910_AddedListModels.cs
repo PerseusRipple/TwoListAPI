560d33104f688c390b06ceebd42e7aa7a92964cf
PerseusRipple <perseus.ripple@gmail.com>
2019-03-29 13:10:40 -0400

    added controllers and models together

---
+++ Migrations/20190329170910_AddedListModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace twolistapi.Migrations
+{
+    public partial class AddedListModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    ListItem = table.Column<string>(nullable: true),
+                    IsDone = table.Column<bool>(nullable: false),
+                    DateOfCompletion = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lists", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lists");
+        }
+    }
+}
