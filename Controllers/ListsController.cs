ed437e8a41b1b4662796c23f02e993e005117914
Will Warren <willwarrenmail@gmail.com>
2019-03-31 14:41:57 -0400

    attempting to fix merge issue, issue not shown

--- Controllers/ListsController.cs
+++ Controllers/ListsController.cs
@@ -58,8 +58,8 @@ namespace dotnet_sdg_template.Controllers
         await _context.SaveChangesAsync();
 <<<<<<< HEAD
 =======
-
 >>>>>>> a311188ae7b68ecaf6e83ecadce7a34cea7796af
+
         return Ok();
       }
       catch (DbUpdateConcurrencyException)
